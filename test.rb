fba052dc46e8b6babcdb6ff66f51aede3f16046e
Bernardngari <bernardngari.ke@gmail.com>
2022-08-16 21:39:23 +0300

    commit tree b9088eccda3253075a147d43c508a6ac2d60c5eb

---
+++ test.rb
@@ -0,0 +1,10 @@
+def reverse_string(str)
+  #reversed_str = ""
+  str.length.times do |i|
+    #reversed_str = str[i] + reversed_str
+		"My value is str[i]"
+  end
+  #reversed_str
+	#"my value is #{i}"
+end
+reverse_string("kahuthu")
